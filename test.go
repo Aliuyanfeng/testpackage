76f0e93e9f4fe221729288527e0ed3b353a0f323
LiuYanFeng <1773189480@qq.com>
2024-01-15 17:29:56 +0800

    Initial commit

---
+++ test.go
@@ -0,0 +1,23 @@
+package testpackage
+
+// Hello returns a greeting.
+func Hello() string {
+	return "Hello Everyone !"
+}
+
+// Glass returns a useful phrase for world travelers.
+func Glass() string {
+	// See http://www.oocities.org/nodotus/hbglass.html.
+	return "I can eat glass and it doesn't hurt me."
+}
+
+// Go returns a Go proverb.
+func Go() string {
+	return "Don't communicate by sharing memory, share memory by communicating."
+}
+
+// Opt returns an optimization truth.
+func Opt() string {
+	// Wisdom from ken.
+	return "If a program is too slow, it must have a loop."
+}
